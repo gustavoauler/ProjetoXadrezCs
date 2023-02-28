61227573271d2dda003f7765ba3a7293c21901c0
gustavoauler <gustavooauler@gmail.com>
2023-02-28 17:36:26 -0300

    Adicionando as demais pecas

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -11,7 +11,7 @@ namespace xadrez_console.tabuleiro
         public Posicao Posicao { get; set; }
         public Cor Cor { get; protected set; }
         public int QteMovimentos { get; protected set; }
-        public Tabuleiro Tabuleiro { get; protected set; }
+        public Tabuleiro Tabuleiro { get; set; }
 
         public Peca(Tabuleiro tabuleiro, Cor cor)
         {
